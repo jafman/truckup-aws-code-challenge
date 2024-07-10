1d8d393b9923a5d52e1d2b132d3c2ef0420cdff9
Jafar Kamarudeen <abduljafar.kamarudeen@mtn.com>
2024-07-10 20:51:24 +0100

    feat: apigw & lambda functions
    Add API Gateway resource, and lamda functions

---
+++ lambda/shedule-manager.js
@@ -0,0 +1,17 @@
+exports.getUserSchedule = async function (event) {
+  console.log("request:", );
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "text/plain" },
+    body: `Hello! You've hit ${event.path}\n ${JSON.stringify(event, undefined, 2)}`
+  };
+}
+
+exports.defaultHandler = async function (event) {
+  console.log("request:", );
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "text/plain" },
+    body: `Hello! You've hit the default handler\n ${JSON.stringify(event, undefined, 2)}`
+  };
+}
--- lib/schedule-manager-stack.ts
+++ lib/schedule-manager-stack.ts
@@ -1,19 +1,44 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+// import * as sns from 'aws-cdk-lib/aws-sns';
+// import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
 
 export class ScheduleManagerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'ScheduleManagerQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // const queue = new sqs.Queue(this, 'ScheduleManagerQueue', {
+    //   visibilityTimeout: Duration.seconds(300)
+    // });
+
+    // const topic = new sns.Topic(this, 'ScheduleManagerTopic');
+
+    // topic.addSubscription(new subs.SqsSubscription(queue));
+    const getUserSchedule = new lambda.Function(this, 'GetUserScheduleHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'shedule-manager.getUserSchedule',
+    })
+
+    const defaultHandler = new lambda.Function(this, 'DefaultHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'shedule-manager.defaultHandler',
+    })
+
+    const apiGateway = new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: defaultHandler,
+      proxy: false,
+      defaultMethodOptions: {
+        apiKeyRequired: false
+      }
     });
 
-    const topic = new sns.Topic(this, 'ScheduleManagerTopic');
+    const user = apiGateway.root.addResource('user').addResource('schedule');
+    user.addMethod('GET', new apigw.LambdaIntegration(getUserSchedule));
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
